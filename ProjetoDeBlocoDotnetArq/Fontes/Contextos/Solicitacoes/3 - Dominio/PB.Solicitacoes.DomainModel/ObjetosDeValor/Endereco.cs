5f2dd2d1f0aa08d82e7233f7529f52f0942cb24b
Tiago Oliveira <tiagooliveira@MacBook-Pro-de-Tiago.local>
2018-06-24 10:01:46 -0300

    Comitar Mapeamento das entidades com entity framework core

--- ProjetoDeBlocoDotnetArq/Fontes/Contextos/Solicitacoes/3 - Dominio/PB.Solicitacoes.DomainModel/ObjetosDeValor/Endereco.cs
+++ ProjetoDeBlocoDotnetArq/Fontes/Contextos/Solicitacoes/3 - Dominio/PB.Solicitacoes.DomainModel/ObjetosDeValor/Endereco.cs
@@ -3,8 +3,25 @@ namespace PB.Solicitacoes.DomainModel.ObjetosDeValor
 {
     public class Endereco
     {
-        public Endereco()
+        protected Endereco() { }
+
+        public Endereco(string cep, string logradouro, int numero, int? complemento, string bairro, string cidade, string estado)
         {
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
         }
+
+        public string Cep { get; private set; }
+        public string Logradouro { get; private set; }
+        public int Numero { get; private set; }
+        public int? Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
     }
 }
