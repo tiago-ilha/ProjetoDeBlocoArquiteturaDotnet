644686966e3504fb0861fd08b68e5d02fb596d27
Tiago Oliveira <tiagooliveira@MacBook-Pro-de-Tiago.local>
2018-06-25 00:51:11 -0300

    Testando nova configuração para fazer o nhibernate funcionar

---
+++ ProjetoDeBlocoDotnetArq/Fontes/Contextos/Solicitacoes/4 - Infra/PB.Solicitacoes.Infra/Configuracoes/SessionFactoryProvider.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace PB.Solicitacoes.Infra.Configuracoes
+{
+    public interface SessionFactoryProvider
+    {
+    }
+}
---
+++ ProjetoDeBlocoDotnetArq/Fontes/Contextos/Solicitacoes/4 - Infra/PB.Solicitacoes.Infra/Configuracoes/SessionProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PB.Solicitacoes.Infra.Configuracoes
+{
+    public class SessionProvider
+    {
+        public SessionProvider()
+        {
+        }
+    }
+}
